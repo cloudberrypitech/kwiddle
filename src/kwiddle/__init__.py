7b82a38f420b6718bd371b9cb80b0d4ae69a2f1a
CloudberryPiTech <bubblesbakale@outlook.com>
2026-09-17 21:40:17 +0530

    Update __init__.py Added default variables and default startup functions code

--- src/kwiddle/__init__.py
+++ src/kwiddle/__init__.py
@@ -1,8 +1,30 @@
 # Imports
 from tkinter import *
+from tkinter import messagebox
+from subprocess import call
 
 # Window Attributes
 window = Tk()
 window.geometry("500x500")
 window.title("Kwiddle")
-window.iconphoto(PhotoImage("winlogo.png"))
+window.iconphoto(True, PhotoImage("winlogo.png"))
+
+# Variables
+star_repo = None
+repo_path:str = "https://github.com/cloudberrypitech/kwiddle"
+
+# Default Startup Functions
+def contribute_repo():
+    global star_repo
+    star_repo = messagebox.askyesno(parent=window, title="Contribute", message="Do you want to star the Kwiddle repo on GitHub?")
+    if star_repo:
+        call(f"open {repo_path}", shell=True)
+        star_repo = True
+        messagebox.showinfo(parent=window, icon="info", title="Contribute", message="Thank you for contributing to Kwiddle.")
+    else:
+        star_repo = False
+
+# Startup Functions Execution Script
+contribute_repo()
+
+mainloop()
