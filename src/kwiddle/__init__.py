e6f930cfc45b3408aa08e27d6c76d71f7ccd8a5b
CloudberryPiTech <bubblesbakale1@outlook.com>
2026-09-18 20:57:20 +0530

    Created basic book test.
    Created basic book test with book layout in a list.

--- src/kwiddle/__init__.py
+++ src/kwiddle/__init__.py
@@ -10,6 +10,7 @@ window.title("Kwiddle")
 window.iconphoto(True, PhotoImage("winlogo.png"))
 
 # Variables
+page = 0
 star_repo = None
 repo_path:str = "https://github.com/cloudberrypitech/kwiddle"
 
@@ -25,6 +26,71 @@ def contribute_repo():
         star_repo = False
 
 # Startup Functions Execution Script
-contribute_repo()
+#contribute_repo()
+
+# 3 - 5 Offline Books Content
+clifford = [
+    """Clifford
+       By Norman Bridwell
+       Publishing by : Scholastic""",
+
+    """It's Clifford's bedtime.
+       His mother helps him into bed.""",
+
+    """But Clifford isn't ready.
+       He needs his bear.""",
+
+    """He needs his doll.
+       He needs his blanket.""",
+
+    """Now Clifford needs a drink of water.""",
+
+    """But Clifford isn't sleepy yet.
+       What does Clifford need now?""",
+
+    """Clifford needs his goodnight kiss.""",
+
+    """Sweet dreams,
+        Clifford.""",
+
+    """
+    Clifford
+    
+    THE SMALL RED PUPPY
+    
+    All rights reserved. Published by Scholastic Inc.
+    SCHOLASTIC and associated logos are trademarks and/or
+    registered trademarks of Scholastic Inc.
+    """
+]
+
+def book(bookname, content:list):
+    global page
+    bookwin = Toplevel(window)
+    bookwin.title(f"Kwiddle - {bookname}")
+    text = Text(bookwin)
+    text.pack()
+    text.insert(END, content[page])
+    def nextpage():
+        global page
+        try:
+            page += 1
+            text.delete(1.0, END)
+            text.insert(END, content[page])
+        except IndexError:
+            messagebox.showinfo(parent=bookwin, title="Bookreader", message="Book Completed")
+    Button(bookwin, text="Next", command=nextpage).pack(anchor="ne", padx=10)
+
+    def lastpage():
+        global page
+        try:
+            page -= 1
+            text.delete(1.0, END)
+            text.insert(END, content[page])
+        except IndexError:
+            messagebox.showinfo(parent=bookwin, title="Bookreader", message="Book Completed")
+    Button(bookwin, text="Previous", command=lastpage).pack(anchor="nw", pady=0, padx=10)
+
+book(bookname="Clifford's Bedtime", content=clifford)
 
 mainloop()
